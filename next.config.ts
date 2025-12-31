d217665246d1d919f1f259f753b01a55e17318ac
luckylula <lulamartinezperez@gmail.com>
2025-12-31 18:34:06 +0100

    Añadir Unsplash a dominios permitidos para imágenes

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'static.wixstatic.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
