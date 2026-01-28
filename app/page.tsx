64981ff86f9760ef301c782b643242bbaf840c8f
Lula Martinez Perez <lulamartinezperez@gmail.com>
2026-01-28 13:54:11 +0100

    Actualización rápida

--- app/page.tsx
+++ app/page.tsx
@@ -402,7 +402,7 @@ export default function Home() {
           </div>
           <div className="overflow-hidden rounded-lg">
             <Image
-              src="/categorias/somosequipo.png"
+              src="/somosequipo.png"
               alt="Equipo Control Play Sports"
               width={1200}
               height={800}
